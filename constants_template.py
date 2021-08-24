f506430afde9e73eb6d27ca1d0b12d5964d565c9
Aigul <aigul.sibgatulli@mail.ru>
2021-08-24 22:00:02 +0300

    update gitignore

--- constants_template.py
+++
@@ -1,6 +0,0 @@
-# Id пользователя, см. цифры после https://www.kinopoisk.ru/user/
-USER_ID = 15557089
-# Количество просмотренных фильмов
-COUNT = 490
-# Ключ API, см. на вкладке "Profile" после регистрации: https://kinopoiskapiunofficial.tech/
-API_KEY = '5cdb595b-7b01-4cb9-8569-670bc888864b'
